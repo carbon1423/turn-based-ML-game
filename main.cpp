44643828e4152c5061beb4e108078e17350aaf7c
carbon1423 <bocke034@umn.edu>
2025-09-05 14:54:10 -0500

    Add green rectangle

--- main.cpp
+++ main.cpp
@@ -9,12 +9,26 @@ int main() {
                                           800, 600,
                                           SDL_WINDOW_SHOWN);
 
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+
+
     bool running = true;
     SDL_Event e;
     while (running) {
         while (SDL_PollEvent(&e)) {
             if (e.type == SDL_QUIT) running = false;
         }
+
+        SDL_SetRenderDrawColor(renderer, 100, 100, 100, 255); // black
+        SDL_RenderClear(renderer);
+
+        SDL_SetRenderDrawColor(renderer, 100, 200, 70, 175);
+
+        SDL_Rect rect = {200, 150, 400, 300};
+        SDL_RenderFillRect(renderer, &rect);
+
+        SDL_RenderPresent(renderer);
     }
 
     SDL_DestroyWindow(window);

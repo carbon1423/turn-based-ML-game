3a9983e303080177b851b51855abbc4fdee8ccc6
carbon1423 <bocke034@umn.edu>
2025-09-05 17:45:10 -0500

    Add basic menu and button UI, Need to implement functionality

--- main.cpp
+++ main.cpp
@@ -1,4 +1,14 @@
 #include <SDL2/SDL.h>
+#define WIN_HEIGHT 600
+#define WIN_LENGTH 900
+
+enum GameState {
+    MAIN_MENU,
+    MOVE_MENU,
+    TURN_EXECUTION,
+    GAME_OVER
+};
+
 
 int main() {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) return 1;
@@ -6,13 +16,13 @@ int main() {
     SDL_Window* window = SDL_CreateWindow("My SDL2 Window",
                                           SDL_WINDOWPOS_CENTERED,
                                           SDL_WINDOWPOS_CENTERED,
-                                          800, 600,
+                                          WIN_LENGTH, WIN_HEIGHT,
                                           SDL_WINDOW_SHOWN);
 
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
 
-
+    GameState current_state = MAIN_MENU;
     bool running = true;
     SDL_Event e;
     while (running) {
@@ -20,14 +30,24 @@ int main() {
             if (e.type == SDL_QUIT) running = false;
         }
 
-        SDL_SetRenderDrawColor(renderer, 100, 100, 100, 255); // black
+        SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255); // dark gray
         SDL_RenderClear(renderer);
 
-        SDL_SetRenderDrawColor(renderer, 100, 200, 70, 175);
 
-        SDL_Rect rect = {200, 150, 400, 300};
-        SDL_RenderFillRect(renderer, &rect);
+        // Create the menu area (Gray)
+        SDL_SetRenderDrawColor(renderer, 100, 100, 100, 255);
+        SDL_Rect menuBack = {0, (3*WIN_HEIGHT)/4, WIN_LENGTH, WIN_HEIGHT/4};
+        SDL_RenderFillRect(renderer, &menuBack);
+        if(current_state == MAIN_MENU){
+            // Create the fight button and the item button (red and green for now)
+            SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // Red for fight button
+            SDL_Rect fightButton = {WIN_LENGTH/9, (3*WIN_HEIGHT)/4 + WIN_HEIGHT/24, WIN_LENGTH/3, WIN_LENGTH/9};
+            SDL_RenderFillRect(renderer, &fightButton);
 
+            SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255); // Green for item button
+            SDL_Rect itemButton = {5*WIN_LENGTH/9, (3*WIN_HEIGHT)/4 + WIN_HEIGHT/24, WIN_LENGTH/3, WIN_LENGTH/9};
+            SDL_RenderFillRect(renderer, &itemButton);
+        }
         SDL_RenderPresent(renderer);
     }
 
